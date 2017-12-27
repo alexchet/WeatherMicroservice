ef3aa39d92bea0fe7533f6f126c1222e91f9601c
Alex <chetcuti.alex@gmail.com>
2017-12-27 18:47:23 +0100

    Weather Report class

---
+++ WeatherReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMicroservice
+{
+    public class WeatherReport
+    {
+        private static readonly string[] PossibleConditions = new string[]
+        {
+            "Sunny",
+            "Mostly Sunny",
+            "Partly Sunny",
+            "Partly Cloudy",
+            "Mostly Cloudy",
+            "Rain"
+        };
+
+        public int HiTemperature { get; }
+        public int LoTemperature { get; }
+        public int AverageWindSpeed { get; }
+        public string Conditions { get; }
+    }
+}
