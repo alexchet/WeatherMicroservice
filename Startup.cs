56bf745874d93a2ef269c2eff416bcdfa7e75db8
Alex <chetcuti.alex@gmail.com>
2017-12-27 18:42:00 +0100

    TryParse extenstion to return nullable double, retrieving query string

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,13 @@ namespace WeatherMicroservice
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var latString = context.Request.Query["lat"].FirstOrDefault();
+                var longString = context.Request.Query["long"].FirstOrDefault();
+
+                var latitude = latString.TryParse();
+                var longitude = longString.TryParse();
+
+                await context.Response.WriteAsync($"Retrieving Weather for lat: {latitude}, long: {longitude}");
             });
         }
     }
